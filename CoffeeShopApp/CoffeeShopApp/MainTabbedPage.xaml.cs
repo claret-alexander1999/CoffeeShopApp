1b26af4ab439aeadc9163655fc065165778b7714
claret-alexander1999 <73807716+claret-alexander1999@users.noreply.github.com>
2021-10-31 22:52:37 +1300

    CoffeeShopApp Complete Project

---
+++ CoffeeShopApp/CoffeeShopApp/MainTabbedPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CoffeeShopApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainTabbedPage : TabbedPage
+    {
+        public MainTabbedPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void EspressoButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EspressoPage());
+        }
+
+        private async void HotDrinksButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HotDrinksPage());
+
+        }
+
+        private async void ColdDrinksButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ColdDrinksPage());
+
+        }
+
+        private async void QuickBitesButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new QuickBitesPage());
+
+        }
+    }
+}
